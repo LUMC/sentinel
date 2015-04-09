ad2cb67c50b2d4aac390f48bf31058721e68dd06
bow <bow@bow.web.id>
2015-04-09 18:05:51 +0200

    Add package-level object to store various version infos and supported pipelines

---
+++ src/main/scala/nl/lumc/sasc/sentinel/package.scala
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2015 Leiden University Medical Center - Sequencing Analysis Support Core <sasc@lumc.nl>
+ *
+ * @author Wibowo Arindrarto <w.arindrarto@lumc.nl>
+ */
+package nl.lumc.sasc
+
+/** General package-level information */
+package object sentinel {
+
+  /** Supported schema versions */
+  object SchemaVersion extends Enumeration {
+    type SchemaVersion = Value
+    val V04 = Value
+  }
+
+  /** Current schema versions */
+  val CurrentSchemaVersion = SchemaVersion.V04
+
+  /** Supported pipeline summary schemas */
+  object Pipeline extends Enumeration {
+    type Pipeline = Value
+    val Gentrap = Value
+  }
+
+  /** Current API version */
+  val CurrentApiVersion = "0.1.0"
+}
