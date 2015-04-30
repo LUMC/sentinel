8e7b0fe350247c18ba5047fd4f17583d3351c643
bow <bow@bow.web.id>
2015-04-30 12:05:21 +0200

    Remove annotations endpoint from refs endpoint

--- src/main/scala/nl/lumc/sasc/sentinel/api/RefsController.scala
+++ src/main/scala/nl/lumc/sasc/sentinel/api/RefsController.scala
@@ -15,7 +15,7 @@ class RefsController(implicit val swagger: Swagger) extends ScalatraServlet
 
   protected implicit val jsonFormats: Formats = DefaultFormats
 
-  protected val applicationDescription: String = "reference and annotation information"
+  protected val applicationDescription: String = "Retrieval of reference sequence synopses"
   override protected val applicationName: Option[String] = None
 
   before() {
@@ -23,17 +23,25 @@ class RefsController(implicit val swagger: Swagger) extends ScalatraServlet
     response.headers += ("Access-Control-Allow-Origin" -> "*")
   }
 
-  val referencesGetOperation = (apiOperation[List[AlignmentReference]]("referencesGet")
-    summary "Retrieves all available reference items."
-    )
-
-  get("/", operation(referencesGetOperation)) {
-  }
-
-  val annotationsGetOperation = (apiOperation[List[AnnotationFile]]("annotationsGet")
-    summary "Retrieves all available annotation items."
+  val referencesRefIdGetOperation = (apiOperation[List[Reference]]("referencesRefIdGet")
+    summary "Retrieves a single full reference item."
+    parameters pathParam[String]("refId").description("Reference ID query.")
+    responseMessages (
+      StringResponseMessage(400, "Reference ID not specified."),
+      StringResponseMessage(404, "Reference ID can not be found."))
   )
 
-  get("/annotations", operation(annotationsGetOperation)) {
+  get("/:refId", operation(referencesRefIdGetOperation)) {
+    val refId = params.getAs[String]("refId").getOrElse(halt(400, ApiError("Reference ID not specified.")))
+    // TODO: return 404 if reference ID is not found
+    // TODO: return 200 and reference item
+  }
+
+  val referencesGetOperation = (apiOperation[List[Reference]]("referencesGet")
+    summary "Retrieves all available reference items."
+  )
+
+  get("/", operation(referencesGetOperation)) {
+    // TODO: return 200 and reference items
   }
 }
