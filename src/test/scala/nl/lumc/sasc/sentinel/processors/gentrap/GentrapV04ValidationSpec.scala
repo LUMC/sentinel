26a2b984a8779293e78f12ad3b4cf8003eefda0f
bow <bow@bow.web.id>
2015-05-11 22:08:35 +0200

    Update validation test

--- src/test/scala/nl/lumc/sasc/sentinel/processors/gentrap/GentrapV04ValidationSpec.scala
+++ src/test/scala/nl/lumc/sasc/sentinel/processors/gentrap/GentrapV04ValidationSpec.scala
@@ -3,21 +3,24 @@
  *
  * @author Wibowo Arindrarto <w.arindrarto@lumc.nl>
  */
-package nl.lumc.sasc.sentinel.adapters.gentrap
+package nl.lumc.sasc.sentinel.processors.gentrap
 
 import org.specs2._
 import org.specs2.mock.Mockito
 
-import nl.lumc.sasc.sentinel.validation.SchemaValidationSpec
+import nl.lumc.sasc.sentinel.JsonLoaderSpec
+import nl.lumc.sasc.sentinel.db.MongodbAccessObject
 
-class GentrapV04ValidationSpec extends Specification with SchemaValidationSpec with Mockito { def is = s2"""
+class GentrapV04ValidationSpec extends Specification with JsonLoaderSpec with Mockito { def is = s2"""
 
   The Gentrap v0.4 schema must
     be valid for multi sample, single library summary              $multiSampleSingleLibV04
     be valid for multi sample, multi library summary               $multiSampleMultiLibV04
 """
 
-  val gentrapV04InputProcessor = new GentrapV04InputProcessor(mockDb)
+  val mockConn = mock[MongodbAccessObject]
+
+  val gentrapV04InputProcessor = new GentrapV04InputProcessor(mockConn)
 
   def multiSampleSingleLibV04 = {
     val summary = loadJson("/v0.4/gentrap_multi_sample_single_lib.json")
