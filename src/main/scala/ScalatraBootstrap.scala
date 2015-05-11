3b6dad0b567fcfbaca597c736fd8980ac7f8b865
bow <bow@bow.web.id>
2015-05-11 22:17:29 +0200

    Update controllers with db access object argument

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -1,20 +1,27 @@
 import akka.actor.ActorSystem
+import com.mongodb.casbah.MongoClient
 import javax.servlet.ServletContext
 import org.scalatra.LifeCycle
 
 import nl.lumc.sasc.sentinel.api._
+import nl.lumc.sasc.sentinel.db.MongodbAccessObject
 
 class ScalatraBootstrap extends LifeCycle {
+
   implicit val swagger = new SentinelSwagger
 
+  val conn = MongodbAccessObject(MongoClient("localhost", 27017), "sentinel")
+
   override def init(context: ServletContext) {
+
     implicit val system = ActorSystem("appActorSystem")
+
     try {
       context mount (new RootController, "/*")
       context mount (new StatsController, "/stats/*")
       context mount (new ReferencesController, "/references/*")
-      context mount (new AnnotationsController, "/annotations/*")
-      context mount (new RunsController, "/runs/*")
+      context mount (new AnnotationsController(conn), "/annotations/*")
+      context mount (new RunsController(conn), "/runs/*")
       context mount (new UsersController, "/users/*")
       context mount (new ResourcesApp, "/api-docs/*")
     } catch {
