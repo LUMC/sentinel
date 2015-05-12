de931d8b5dfec8bd4dd76af6cfb2e50b171e1e1d
bow <bow@bow.web.id>
2015-05-12 19:30:28 +0200

    Update AnnotationsProcessor

--- src/main/scala/nl/lumc/sasc/sentinel/processors/AnnotationsProcessor.scala
+++ src/main/scala/nl/lumc/sasc/sentinel/processors/AnnotationsProcessor.scala
@@ -1,7 +1,3 @@
-/**
- * Copyright (c) 2015 Leiden University Medical Center - Sequencing Analysis Support Core <sasc@lumc.nl>
- * @author Wibowo Arindrarto <w.arindrarto@lumc.nl>
- */
 package nl.lumc.sasc.sentinel.processors
 
 import nl.lumc.sasc.sentinel.db._
