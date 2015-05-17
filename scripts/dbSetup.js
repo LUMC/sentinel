1eafcd1c15614a425ce605c5a4307a4734c15c33
bow <bow@bow.web.id>
2015-05-17 21:19:58 +0200

    Update dbSetup.js script to also add MongoDB users and remove existing setup

--- scripts/dbSetup.js
+++ scripts/dbSetup.js
@@ -1,28 +1,57 @@
 // dbSetup.js
 //
-// Script for initial Sentinel MongoDB database setup
+// Script for initial Sentinel MongoDB database setup for development.
+//
+// This script creates the required indices and users. Notably,
+// it adds two MongoDB users with the following user names:
+//
+//   - sentinel-owner (with dbOwner role of the sentinel database)
+//   - sentinel-api (with readWrite role of the sentinel database)
+//
+// The sentinel-owner is meant for administering the database, while
+// sentinel-api is how the Sentinel API accesses the database. Both accounts
+// initially have the same passwords as their usernames. If you intend to
+// deploy later, we strongly suggest that these passwords be changed.
+//
+// The script must be run by an existing MongoDB user that can create
+// databases and add users. It will overwrite any existing data for Sentinel,
+// so use it carefully.
 //
 // Usage: mongo {address}:{port} dbSetup.js
 
-// Don't bootstrap if database already has content
+print("\n*** Bootstrapping Sentinel ***");
+
 if (db.getCollectionNames().length > 0) {
-    throw "ERROR: Sentinel database is already created. Exiting.";
+    print("\nRemoving existing Sentinel setup ...");
+    db.dropDatabase();
 }
 
-print("\n    *** Bootstrapping Sentinel ***\n");
-
-print("    Creating indices for ...");
+print("\nCreating indices for ...");
 // fs.files -> index by md5 and metadata.uploader + unique
 db.fs.files.createIndex({"md5": 1, "metadata.uploader": 1}, {"unique": true})
-print("      - raw uploads");
+print("- raw uploads");
 // annotations -> annotMd5 + unique
 db.annotations.createIndex({"annotMd5": 1}, {"unique": true})
-print("      - annotation records");
+print("- annotation records");
 // references -> combinedMd5 + unique
 db.references.createIndex({"combinedMd5": 1}, {"unique": true})
-print("      - reference records");
+print("- reference records");
 
-print("    Adding the admin user ...");
+print("\nAdding MongoDB users ...");
+db.dropUser("sentinel-owner");
+db.createUser({
+    user: "sentinel-owner",
+    pwd: "sentinel-owner",
+    roles: [ "dbOwner" ]
+});
+db.dropUser("sentinel-api");
+db.createUser({
+    user: "sentinel-api",
+    pwd: "sentinel-api",
+    roles: [ "readWrite" ]
+});
+
+print("\nAdding the API admin user ...");
 // NOTE: must be kept in sync with User in the source code
 var adminUser = {
     id: "admin",
@@ -33,4 +62,4 @@ var adminUser = {
 }
 db.user.insert(adminUser);
 
-print("\n    ************ Done ************\n");
+print("\n************ Done ************\n");
