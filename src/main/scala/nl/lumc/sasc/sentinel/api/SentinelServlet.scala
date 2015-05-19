0cd1abb035fdfbf7649d4a674080162e2b9c13c7
bow <bow@bow.web.id>
2015-05-19 12:07:19 +0200

    Intercept ObjectId Swagger spec model creation to prevent internal attributes exposure

--- src/main/scala/nl/lumc/sasc/sentinel/api/SentinelServlet.scala
+++ src/main/scala/nl/lumc/sasc/sentinel/api/SentinelServlet.scala
@@ -6,8 +6,9 @@ import org.json4s.mongo.ObjectIdSerializer
 
 import org.scalatra.ScalatraServlet
 import org.scalatra.json.JacksonJsonSupport
+import org.scalatra.swagger.{ DataType, Model, SwaggerSupport }
 
-abstract class SentinelServlet extends ScalatraServlet with JacksonJsonSupport {
+abstract class SentinelServlet extends ScalatraServlet with JacksonJsonSupport with SwaggerSupport {
 
   protected class SentinelObjectIdSerializer extends ObjectIdSerializer {
 
@@ -20,4 +21,30 @@ abstract class SentinelServlet extends ScalatraServlet with JacksonJsonSupport {
     formats.emptyValueStrategy.replaceEmpty(value)
 
   protected implicit val jsonFormats: Formats = DefaultFormats + new SentinelObjectIdSerializer
+
+  override protected def registerModel(model: Model): Unit = {
+    // FIXME: This is a bit hackish, but scalatra-swagger does not make it clear how to intercept / prevent certain
+    //        models from being exposed. Until they have an officially documented way of doing so, we'll stick with this.
+    // Intercept ObjectId model creation, to prevent its internal attributes from being exposed in the API
+    // We only want to show them as plain strings
+    // Also, skip ObjectId creation completely
+    if (model.id != "ObjectId") {
+      val interceptedProp = model.properties.map {
+        case (propName, prop) =>
+          val interceptedProp = prop.`type` match {
+            case vdt: DataType.ValueDataType if vdt.name == "ObjectId" =>
+              prop.copy(`type` = DataType.String)
+            case cdt: DataType.ContainerDataType if cdt.typeArg.isDefined && cdt.typeArg.get.name == "ObjectId" =>
+              cdt.name match {
+                case "List"    => prop.copy(`type` = DataType.GenList(DataType.String))
+                case "Set"     => prop.copy(`type` = DataType.GenSet(DataType.String))
+                case otherwise => prop
+              }
+            case otherwise => prop
+          }
+          (propName, interceptedProp)
+      }
+      super.registerModel(model.copy(properties = interceptedProp))
+    }
+  }
 }
