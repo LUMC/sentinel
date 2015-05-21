55236b1af46a8bed2539c997fa7ce258d232af16
bow <bow@bow.web.id>
2015-05-21 21:28:13 +0200

    Add adapter for user access

---
+++ src/main/scala/nl/lumc/sasc/sentinel/db/UsersAdapter.scala
@@ -0,0 +1,20 @@
+package nl.lumc.sasc.sentinel.db
+
+import com.mongodb.casbah.Imports._
+import com.novus.salat._
+import com.novus.salat.global._
+
+import nl.lumc.sasc.sentinel.models.User
+
+trait UsersAdapter { this: MongodbConnector =>
+
+  val usersCollectionName = CollectionNames.Users
+
+  private lazy val coll = mongo.db(usersCollectionName)
+
+  def userExist(userId: String): Boolean = coll.find(MongoDBObject("id" -> userId)).limit(1).size == 1
+
+  def addUser(user: User): Unit = coll.insert(grater[User].asDBObject(user))
+
+  def deleteUser(userId: String): Unit = coll.remove(MongoDBObject("id" -> userId))
+}
