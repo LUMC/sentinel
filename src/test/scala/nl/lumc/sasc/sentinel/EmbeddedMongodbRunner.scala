a02eb882c2ff1466bf01bdfa98e8703e81fd94a0
bow <bow@bow.web.id>
2015-05-21 21:26:59 +0200

    Add embedded mongo for testing

---
+++ src/test/scala/nl/lumc/sasc/sentinel/EmbeddedMongodbRunner.scala
@@ -0,0 +1,53 @@
+package nl.lumc.sasc.sentinel
+
+import scala.annotation.tailrec
+import scala.util.{ Failure, Success, Try }
+
+import com.mongodb.casbah.Imports._
+import de.flapdoodle.embed.mongo.{ Command, MongodStarter }
+import de.flapdoodle.embed.mongo.config._
+import de.flapdoodle.embed.mongo.distribution.Version
+import de.flapdoodle.embed.process.config.io.ProcessOutput
+import de.flapdoodle.embed.process.runtime.Network
+import org.slf4j.helpers.NOPLoggerFactory
+
+import nl.lumc.sasc.sentinel.db.MongodbAccessObject
+
+trait EmbeddedMongodbRunner {
+
+  protected val mongodVersion = Version.Main.V3_0
+
+  protected val dbName = "sentinel_testing"
+
+  private lazy val mongodPort: Int = {
+
+    @tailrec def getFreePort: Int = Try(Network.getFreeServerPort) match {
+      case Success(port) => port
+      case Failure(_)    => getFreePort
+    }
+
+    getFreePort
+  }
+
+  private val runtimeConfig = new RuntimeConfigBuilder()
+    .defaultsWithLogger(Command.MongoD, (new NOPLoggerFactory).getLogger("nop"))
+    .processOutput(ProcessOutput.getDefaultInstanceSilent)
+    .build()
+
+  private val mongodConfig = new MongodConfigBuilder()
+    .version(mongodVersion)
+    .net(new Net(mongodPort, Network.localhostIsIPv6))
+    .build()
+
+  private lazy val starter = MongodStarter.getInstance(runtimeConfig)
+
+  private lazy val mongodExecutable = starter.prepare(mongodConfig)
+
+  protected lazy val mongoClient = MongoClient("localhost", mongodPort)
+
+  protected def makeDbAccess = MongodbAccessObject(mongoClient, dbName)
+
+  def start(): Unit = mongodExecutable.start()
+
+  def stop(): Unit = mongodExecutable.stop()
+}
