8ad238ee717d1560469ffc593fcf47102fdc38da
bow <bow@bow.web.id>
2015-05-21 21:27:41 +0200

    Refactor common sentinel servlet testing functions

---
+++ src/test/scala/nl/lumc/sasc/sentinel/SentinelServletSpec.scala
@@ -0,0 +1,31 @@
+package nl.lumc.sasc.sentinel
+
+import scala.util.Try
+
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
+import org.scalatra.test.specs2.ScalatraSpec
+
+import nl.lumc.sasc.sentinel.models.ApiError
+import nl.lumc.sasc.sentinel.utils.CustomObjectIdSerializer
+
+trait SentinelServletSpec extends ScalatraSpec with EmbeddedMongodbRunner {
+
+  sequential
+
+  override def start() = {
+    super[ScalatraSpec].start()
+    super[EmbeddedMongodbRunner].start()
+  }
+
+  override def stop() = {
+    super[ScalatraSpec].stop()
+    super[EmbeddedMongodbRunner].stop()
+  }
+
+  implicit val formats = DefaultFormats + new CustomObjectIdSerializer
+
+  def jsonBody: Option[JValue] = Try(parse(body)).toOption
+
+  def apiError: Option[ApiError] = jsonBody.collect { case json => json.extract[ApiError] }
+}
