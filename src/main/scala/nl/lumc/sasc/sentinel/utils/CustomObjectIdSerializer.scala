c4e483ff5eb9c5a8363c0199392e3869e6c415e9
bow <bow@bow.web.id>
2015-05-25 17:39:39 +0200

    Add deserializer that can handle ObjectIds without  key

--- src/main/scala/nl/lumc/sasc/sentinel/utils/CustomObjectIdSerializer.scala
+++ src/main/scala/nl/lumc/sasc/sentinel/utils/CustomObjectIdSerializer.scala
@@ -7,6 +7,15 @@ import org.json4s.mongo.ObjectIdSerializer
 
 class CustomObjectIdSerializer extends ObjectIdSerializer {
 
+  private val ObjectIdClass = classOf[ObjectId]
+
+  override def deserialize(implicit format: Formats): PartialFunction[(TypeInfo, JValue), ObjectId] = {
+    case (TypeInfo(ObjectIdClass, _), json) => json match {
+      case JString(s) if ObjectId.isValid(s) => new ObjectId(s)
+      case JObject(JField("$oid", JString(s)) :: Nil) if ObjectId.isValid(s) => new ObjectId(s)
+      case x => throw new MappingException(s"Can't convert $x to ObjectId")
+    }
+  }
   override def serialize(implicit formats: Formats): PartialFunction[Any, JValue] = {
     case x: ObjectId => JString(x.toString)
   }
