521638d755279083d8876167da2a1942278eab72
bow <bow@bow.web.id>
2015-06-03 22:39:02 +0200

    Add tests for GET /stats/alignments/gentrap after multiple uploads

--- src/it/scala/nl/lumc/sasc/sentinel/api/StatsControllerSpec.scala
+++ src/it/scala/nl/lumc/sasc/sentinel/api/StatsControllerSpec.scala
@@ -19,6 +19,47 @@ class StatsControllerSpec extends SentinelServletSpec {
   addServlet(statsServlet, s"$baseEndpoint/*")
   addServlet(runsServlet, "/runs/*")
 
+  class StatsAlnGentrapOkTests(val request: () => ClientResponse, val expNumItems: Int) extends Context.PriorRequests {
+
+    def priorRequests = Seq(request)
+
+    "return status 200" in {
+      priorResponse.status mustEqual 200
+    }
+
+    s"return a JSON list containing $expNumItems objects" in {
+      priorResponse.contentType mustEqual "application/json"
+      priorResponse.jsonBody must haveSize(expNumItems)
+    }
+
+    "each of which" should {
+      Range(0, expNumItems) foreach { idx =>
+        val item = idx + 1
+        s"have the expected attributes (object #$item)" in {
+          priorResponse.body must /#(idx) /("nReads" -> bePositiveNum)
+          priorResponse.body must /#(idx) /("pctChimeras" -> bePositiveNum)
+          priorResponse.body must /#(idx) /("nSingletons" -> bePositiveNum)
+          priorResponse.body must /#(idx) /("maxInsertSize" -> bePositiveNum)
+          priorResponse.body must /#(idx) /("medianInsertSize" -> bePositiveNum)
+          priorResponse.body must /#(idx) /("stdevInsertSize" -> bePositiveNum)
+          priorResponse.body must /#(idx) /("nReadsAligned" -> bePositiveNum)
+          priorResponse.body must /#(idx) /("rateReadsMismatch" -> bePositiveNum)
+          priorResponse.body must /#(idx) /("rateIndel" -> bePositiveNum)
+          priorResponse.body must /#(idx) /("nBasesAligned" -> bePositiveNum)
+          priorResponse.body must /#(idx) /("nBasesUtr" -> bePositiveNum)
+          priorResponse.body must /#(idx) /("nBasesCoding" -> bePositiveNum)
+          priorResponse.body must /#(idx) /("nBasesIntron" -> bePositiveNum)
+          priorResponse.body must /#(idx) /("nBasesIntergenic" -> bePositiveNum)
+          priorResponse.body must /#(idx) /("median5PrimeBias" -> bePositiveNum)
+          priorResponse.body must /#(idx) /("median3PrimeBias" -> bePositiveNum)
+          // TODO: use raw JSON matchers when we upgrade specs2
+          priorResponse.jsonBody must beSome.like { case json =>
+            (json(idx) \ "normalizedTranscriptCoverage").extract[Seq[Long]].size must beGreaterThan(idx)
+          }
+        }
+      }
+    }
+  }
   // FIXME: Since specs2 converts all JsonNumber to Doubles, we have to do the comparison as doubles as well
   def bePositiveNum = beGreaterThan(0: Double) ^^ { (t: String) => t.toDouble }
 
@@ -72,48 +113,45 @@ class StatsControllerSpec extends SentinelServletSpec {
 
         "when using the default parameter should" >> inline {
 
-          new Context.PriorRequests {
+          new StatsAlnGentrapOkTests(() => get(endpoint) { response }, 2)
+        }
+      }
+    }
 
-            def request = () => get(endpoint) { response }
-            override def priorRequests: Seq[Req] = Seq(request)
+    "using multiple gentrap files uploaded by different users" >> inline {
 
-            "return status 200" in {
-              priorResponse.status mustEqual 200
-            }
+      new Context.PriorRequestsClean {
 
-            "return a JSON list containing 2 objects" in {
-              priorResponse.contentType mustEqual "application/json"
-              priorResponse.jsonBody must haveSize(2)
-            }
+        def uploadEndpoint = "/runs"
+        def pipeline = "gentrap"
 
-            "each of which" should {
-              Seq(0, 1) foreach { idx =>
-                val item = idx + 1
-                s"have the expected attributes (object #$item)" in {
-                  priorResponse.body must /#(idx) /("nReads" -> bePositiveNum)
-                  priorResponse.body must /#(idx) /("pctChimeras" -> bePositiveNum)
-                  priorResponse.body must /#(idx) /("nSingletons" -> bePositiveNum)
-                  priorResponse.body must /#(idx) /("maxInsertSize" -> bePositiveNum)
-                  priorResponse.body must /#(idx) /("medianInsertSize" -> bePositiveNum)
-                  priorResponse.body must /#(idx) /("stdevInsertSize" -> bePositiveNum)
-                  priorResponse.body must /#(idx) /("nReadsAligned" -> bePositiveNum)
-                  priorResponse.body must /#(idx) /("rateReadsMismatch" -> bePositiveNum)
-                  priorResponse.body must /#(idx) /("rateIndel" -> bePositiveNum)
-                  priorResponse.body must /#(idx) /("nBasesAligned" -> bePositiveNum)
-                  priorResponse.body must /#(idx) /("nBasesUtr" -> bePositiveNum)
-                  priorResponse.body must /#(idx) /("nBasesCoding" -> bePositiveNum)
-                  priorResponse.body must /#(idx) /("nBasesIntron" -> bePositiveNum)
-                  priorResponse.body must /#(idx) /("nBasesIntergenic" -> bePositiveNum)
-                  priorResponse.body must /#(idx) /("median5PrimeBias" -> bePositiveNum)
-                  priorResponse.body must /#(idx) /("median3PrimeBias" -> bePositiveNum)
-                  // TODO: use raw JSON matchers when we upgrade specs2
-                  priorResponse.jsonBody must beSome.like { case json =>
-                    (json(idx) \ "normalizedTranscriptCoverage").extract[Seq[Long]].size must beGreaterThan(idx)
-                  }
-                }
-              }
-            }
-          }
+        def makeUpload(uploader: User, uploaded: Uploadable): Req = {
+          val params = Seq(("userId", uploader.id), ("pipeline", pipeline))
+          val headers = Map(HeaderApiKey -> uploader.activeKey)
+          () => post(uploadEndpoint, params, Map("run" -> uploaded), headers) { response }
+        }
+
+        def upload1 = makeUpload(Users.admin, SchemaExamples.Gentrap.V04.SSampleMLib)
+        def upload2 = makeUpload(Users.avg, SchemaExamples.Gentrap.V04.MSampleMLib)
+        def upload3 = makeUpload(Users.avg, SchemaExamples.Gentrap.V04.MSampleSLib)
+
+        def priorRequests = Seq(upload1, upload2, upload3)
+
+        "after the first file is uploaded" in {
+          priorResponses.head.status mustEqual 201
+        }
+
+        "after the second file is uploaded" in {
+          priorResponses(1).status mustEqual 201
+        }
+
+        "after the third file is uploaded" in {
+          priorResponses(2).status mustEqual 201
+        }
+
+        "when using the default parameter should" >> inline {
+
+          new StatsAlnGentrapOkTests(() => get(endpoint) { response }, 6)
         }
       }
     }
