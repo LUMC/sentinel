4030543d064f108bff967832a51d6e87bee9f652
bow <bow@bow.web.id>
2015-06-03 15:14:20 +0200

    Refactor sequence stats query to be completely database-side

--- src/main/scala/nl/lumc/sasc/sentinel/processors/gentrap/GentrapOutputProcessor.scala
+++ src/main/scala/nl/lumc/sasc/sentinel/processors/gentrap/GentrapOutputProcessor.scala
@@ -32,33 +32,62 @@ class GentrapOutputProcessor(protected val mongo: MongodbAccessObject) extends M
       .toSeq
   }
 
+  /**
+   * Retrieves sequence statistics from database sample entries.
+   *
+   * Each sample entry in the database contains an array of library entries, which in turn contain the sequence
+   * statistics. So to retrieve the statistics only, this method does some aggregation operations.
+   *
+   * Note that constructing database operations is very prone to runtime errors since we are mostly only stringing
+   * together MongoDBObjects and strings. As such, this method must be tested thoroughly.
+   *
+   * @param libType Library type of the returned sequence statistics.
+   * @param qcPhase Sequencing QC phase of the returned statistics.
+   * @param runs Run IDs of the returned statistics. If not specified, sequence statistics are not filtered by run ID.
+   * @param references Reference IDs of the returned statistics. If not specified, sequence statistics are not filtered
+   *                   by reference IDs.
+   * @param annotations Annotations IDs of the returned statistics. If not specified, sequence statistics are not
+   *                    filtered by annotation IDs.
+   * @return a sequence of [[SeqStats]] objects.
+   */
   def getSeqStats(libType: LibType.Value,
                   qcPhase: SeqQcPhase.Value,
                   runs: Seq[String] = Seq(),
                   references: Seq[String] = Seq(),
                   annotations: Seq[String] = Seq()): Seq[SeqStats] = {
 
-    // TODO: Optimize query to do the filtering and selection on the db-side
-    def libToSeqStats(lib: GentrapLibDocument): Option[SeqStats] =
-      if (qcPhase == SeqQcPhase.Raw) Option(lib.rawStats)
-      else lib.processedStats
+    // Projection operation for retrieving innermost stats object
+    val opProjectStats = {
 
-    def libTypeOk(lib: GentrapLibDocument): Boolean =
-      if (libType == LibType.Paired) lib.rawSeq.read2.isDefined
-      else lib.rawSeq.read2.isEmpty
+      // attrName is the name of the processed/raw sequence objects in the library document
+      val attrName =
+        if (qcPhase == SeqQcPhase.Raw) "rawSeq"
+        else if (qcPhase == SeqQcPhase.Processed) "processedSeq"
+        else throw new RuntimeException("Unexpected sequencing QC phase value: " + qcPhase.toString)
 
-    // TODO: Reduce type case matches ~ prone to runtime errors!
-    def libsFromDbo(dbo: DBObject): Seq[GentrapLibDocument] =
-      dbo.get("libs") match {
-        case libs: BasicDBList => libs
-          .map { case lib: BasicDBObject => grater[GentrapLibDocument].asObject(lib) }
-      }
+      MongoDBObject("$project" ->
+        MongoDBObject(
+          "read1" -> ("$libs." + attrName + ".read1.stats"),
+          "read2" -> ("$libs." + attrName + ".read2.stats")))
+    }
+
+    // Match operation for selecting library type
+    val opMatchLibType =
+      if (libType == LibType.Paired)
+        MongoDBObject("$match" -> MongoDBObject("read2" -> MongoDBObject("$exists" -> true)))
+      else if (libType == LibType.Single)
+        MongoDBObject("$match" -> MongoDBObject("read2" -> MongoDBObject("$exists" -> false)))
+      else
+        throw new RuntimeException("Unexpected library type value: " + libType.toString)
+
+    val operations = Seq(
+      MongoDBObject("$project" -> MongoDBObject("_id" -> 0, "libs" -> 1)),
+      MongoDBObject("$unwind" -> "$libs"),
+      opProjectStats, opMatchLibType)
 
     coll
-      .find(MongoDBObject.empty, MongoDBObject("libs" -> 1))
-      .map { libsFromDbo }.flatten
-      .filter { libTypeOk }
-      .map { libToSeqStats }.flatten
+      .aggregate(operations, AggregationOptions(AggregationOptions.CURSOR))
+      .map { case pstat => grater[SeqStats].asObject(pstat) }
       .toSeq
   }
 }
