b33739d700c77740c691b487043818403fdbc44b
bow <bow@bow.web.id>
2015-06-07 20:46:43 +0200

    Finish GET /annotations spec

--- src/it/scala/nl/lumc/sasc/sentinel/api/AnnotationsControllerSpec.scala
+++ src/it/scala/nl/lumc/sasc/sentinel/api/AnnotationsControllerSpec.scala
@@ -2,6 +2,9 @@ package nl.lumc.sasc.sentinel.api
 
 import org.scalatra.test.Uploadable
 
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
+
 import nl.lumc.sasc.sentinel.{ HeaderApiKey, SentinelServletSpec }
 import nl.lumc.sasc.sentinel.SentinelServletSpec.SchemaExamples
 import nl.lumc.sasc.sentinel.models.User
@@ -19,6 +22,7 @@ class AnnotationsControllerSpec extends SentinelServletSpec {
   addServlet(runsServlet, "/runs/*")
 
   s"GET '$baseEndpoint'" >> {
+    br
 
     val endpoint = baseEndpoint
 
@@ -40,7 +44,52 @@ class AnnotationsControllerSpec extends SentinelServletSpec {
       }
     }
 
-    "using summary files that contain annotation entries should" >> inline {
+    "using a summary file that contain annotation entries" >> inline {
+
+      new Context.PriorRequestsClean {
+
+        def uploadEndpoint = "/runs"
+        def params = Seq(("userId", Users.avg.id), ("pipeline", "gentrap"))
+        def headers = Map(HeaderApiKey -> Users.avg.activeKey)
+        def request = () => post(uploadEndpoint, params,
+          Map("run" -> SchemaExamples.Gentrap.V04.SSampleSLib), headers) { response}
+        def priorRequests = Seq(request)
+
+        "after the run summary file is uploaded" in {
+          priorResponses.head.status mustEqual 201
+        }
+
+        "when using the default parameters should" >> inline {
+
+          new Context.PriorRequests {
+
+            def request = () => get(endpoint) { response }
+            def priorRequests = Seq(request)
+
+            "return status 200" in {
+              priorResponse.status mustEqual 200
+            }
+
+            "return a JSON list containing 3 objects" in {
+              priorResponse.contentType mustEqual "application/json"
+              priorResponse.jsonBody must haveSize(3)
+            }
+
+            "each of which" should {
+              Range(0 ,3) foreach { idx =>
+                val item = idx + 1
+                s"have the expected attributes (object #$item)" in {
+                  priorResponse.body must /#(idx) /("annotId" -> """\S+""".r)
+                  priorResponse.body must /#(idx) /("annotMd5" -> """\S+""".r)
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    "using multiple summary files that contain overlapping annotation entries should" >> inline {
 
       new Context.PriorRequestsClean {
 
@@ -96,6 +145,67 @@ class AnnotationsControllerSpec extends SentinelServletSpec {
                   priorResponse.body must /#(idx) /("annotMd5" -> """\S+""".r)
                 }
               }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  s"GET '$baseEndpoint/:annotId" >> {
+  br
+
+    def endpoint(annotId: String) = s"$baseEndpoint/$annotId"
+
+    "using a run summary file that contain annotation entries" >> inline {
+
+      new Context.PriorRequestsClean {
+
+        def uploadEndpoint = "/runs"
+        def params = Seq(("userId", Users.avg.id), ("pipeline", "gentrap"))
+        def headers = Map(HeaderApiKey -> Users.avg.activeKey)
+        def upload = () => post(uploadEndpoint, params,
+          Map("run" -> SchemaExamples.Gentrap.V04.SSampleSLib), headers) { response}
+        def priorRequests = Seq(upload)
+        def annotIds = (parse(priorResponse.body) \ "annotIds").extract[Seq[String]]
+
+        "after the run summary file is uploaded" in {
+          priorResponse.status mustEqual 201
+        }
+
+        "when a nonexistent annotation entry is queried should" >> inline {
+
+          new Context.PriorRequests {
+
+            def request = () => get(endpoint("yalala")) { response }
+            def priorRequests = Seq(request)
+
+            "return status 404" in {
+              priorResponse.status mustEqual 404
+            }
+
+            "return a JSON object containing the expected message" in {
+              priorResponse.contentType mustEqual "application/json"
+              priorResponse.body must /("message" -> "Annotation ID can not be found.")
+            }
+          }
+        }
+
+        "when an existing annotation entry is queried should" >> inline {
+
+          new Context.PriorRequests {
+
+            def request = () => get(endpoint(annotIds.head)) { response }
+            def priorRequests = Seq(request)
+
+            "return status 200" in {
+              priorResponse.status mustEqual 200
+            }
+
+            "return a JSON object containing the expected attributes" in {
+              priorResponse.contentType mustEqual "application/json"
+              priorResponse.body must /("annotId" -> """\S+""".r)
+              priorResponse.body must /("annotMd5" -> """\S+""".r)
             }
           }
         }
