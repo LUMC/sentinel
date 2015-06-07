af8f15db7092bdbfbcaf621e47156457abea0ab7
bow <bow@bow.web.id>
2015-06-07 22:45:16 +0200

    Update POST /runs tests to use request contexts

--- src/it/scala/nl/lumc/sasc/sentinel/api/UsersControllerSpec.scala
+++ src/it/scala/nl/lumc/sasc/sentinel/api/UsersControllerSpec.scala
@@ -16,57 +16,102 @@ class UsersControllerSpec extends SentinelServletSpec {
   s"POST '$baseEndpoint'" >> {
     br
 
-    "when the user is succesfully created" should {
-      "return status 201 and the correct message" in {
+    val endpoint = baseEndpoint
+
+    "when the user is successfully created should" >> inline {
+
+      new Context.PriorRequestsClean {
+
         val userRequest = UserRequest("yeah", "mail@mail.com", "Mypass123", "Mypass123")
         val payload = toByteArray(userRequest)
-        post(baseEndpoint, payload) {
-          status mustEqual 201
-          body must /("message" -> "New user created.")
-          body must /("data") /("uri" -> ("/users/" + userRequest.id))
-          body must /("data") /("apiKey" -> """\S+""".r)
-        } before {
-          servlet.users.userExist("yeah") must beFalse
-        } after { resetDatabase() }
-      }
-    }
+        def request = () => post(endpoint, payload) { response }
+        def priorRequests = Seq(request)
 
-    "when the request body is empty" should {
-      "return status 400 and the correct message" in {
-        post(baseEndpoint, body = Array.empty[Byte]) {
-          status mustEqual 400
-          body must /("message" -> "Malformed user request.")
+        "return status 201" in {
+          priorResponse.status mustEqual 201
+        }
+
+        "return a JSON object containing the expected message" in {
+          priorResponse.contentType mustEqual "application/json"
+          priorResponse.body must /("message" -> "New user created.")
+          priorResponse.body must /("data") /("uri" -> ("/users/" + userRequest.id))
+          priorResponse.body must /("data") /("apiKey" -> """\S+""".r)
         }
       }
     }
 
-    "when the request body is not valid JSON" should {
-      "return status 400 and the correct message" in {
-        post(baseEndpoint, Array[Byte](10, 20, 30)) {
-          status mustEqual 400
-          body must /("message" -> "Malformed user request.")
+    "when the request body is empty should" >> inline {
+
+      new Context.PriorRequestsClean {
+
+        def request = () => post(endpoint, body = Array.empty[Byte]) { response }
+        def priorRequests = Seq(request)
+
+        "return status 400" in {
+          priorResponse.status mustEqual 400
+        }
+
+        "return a JSON object containing the expected message" in {
+          priorResponse.contentType mustEqual "application/json"
+          priorResponse.body must /("message" -> "Malformed user request.")
         }
       }
     }
 
-    "when the passwords do not match" should {
-      "return status 400 and the correct message" in {
+    "when the request body is not valid JSON should" >> inline {
+
+      new Context.PriorRequestsClean {
+
+        def request = () => post(endpoint, body = Array[Byte](10, 20, 30)) { response }
+        def priorRequests = Seq(request)
+
+        "return status 400" in {
+          priorResponse.status mustEqual 400
+        }
+
+        "return a JSON object containing the expected message" in {
+          priorResponse.contentType mustEqual "application/json"
+          priorResponse.body must /("message" -> "Malformed user request.")
+        }
+      }
+    }
+
+    "when the passwords do not match should" >> inline {
+
+      new Context.PriorRequestsClean {
+
         val payload = toByteArray(UserRequest("yeah", "mail@mail.com", "MyPass123", "Mypass456"))
-        post(baseEndpoint, payload) {
-          status mustEqual 400
-          body must /("message" -> "Invalid user request.")
-          body must /("data") / "Different passwords given."
+        def request = () => post(endpoint, payload) { response }
+        def priorRequests = Seq(request)
+
+        "return status 400" in {
+          priorResponse.status mustEqual 400
+        }
+
+        "return a JSON object containing the expected message" in {
+          priorResponse.contentType mustEqual "application/json"
+          priorResponse.body must /("message" -> "Invalid user request.")
+          priorResponse.body must /("data") / "Different passwords given."
         }
       }
     }
 
-    "when the user ID is less than 3 characters" should {
-      "return status 400 and the correct message" in {
-        val payload = toByteArray(UserRequest("hm", "mail@mail.com", "Mypass123", "Mypass123"))
-        post(baseEndpoint, payload) {
-          status mustEqual 400
-          body must /("message" -> "Invalid user request.")
-          body must /("data") / "User ID shorter than 3 characters."
+    "when the user ID is less than 3 characters should" >> inline {
+
+      new Context.PriorRequestsClean {
+
+        val payload = toByteArray(UserRequest("hm", "mail@mail.com", "MyPass123", "Mypass123"))
+        def request = () => post(endpoint, payload) { response }
+        def priorRequests = Seq(request)
+
+        "return status 400" in {
+          priorResponse.status mustEqual 400
+        }
+
+        "return a JSON object containing the expected message" in {
+          priorResponse.contentType mustEqual "application/json"
+          priorResponse.body must /("message" -> "Invalid user request.")
+          priorResponse.body must /("data") / "User ID shorter than 3 characters."
         }
       }
     }
@@ -74,73 +119,116 @@ class UsersControllerSpec extends SentinelServletSpec {
     "when the user ID contains disallowed characters which" can {
 
       Seq(" ", ".", "*23#%") foreach { nonchar =>
-        s"be '$nonchar'" should {
-          "return status 400 and the correct message" in {
+
+        s"be '$nonchar' should" >> inline {
+          
+          new Context.PriorRequestsClean {
             val payload = toByteArray(UserRequest("yeah" + nonchar, "mail@mail.com", "Mypass123", "Mypass123"))
-            post(baseEndpoint, payload) {
-              status mustEqual 400
-              body must /("message" -> "Invalid user request.")
-              body must /("data") / "User ID contains disallowed characters: .+".r
+            def request = () => post(endpoint, payload) { response }
+            def priorRequests = Seq(request)
+
+            "return status 400" in {
+              priorResponse.status mustEqual 400
+            }
+
+            "return a JSON object containing the expected message" in {
+              priorResponse.body must /("message" -> "Invalid user request.")
+              priorResponse.body must /("data") / "User ID contains disallowed characters: .+".r
             }
           }
         }
       }
     }
 
-    "when the password is less than 6 characters" should {
-      "return status 400 and the correct message" in {
+    "when the password is less than 6 characters should" >> inline {
+
+      new Context.PriorRequestsClean {
         val payload = toByteArray(UserRequest("yeah", "mail@mail.com", "My1aB", "My1aB"))
-        post(baseEndpoint, payload) {
-          status mustEqual 400
-          body must /("message" -> "Invalid user request.")
-          body must /("data") / "Password shorter than 6 characters."
+        def request = () => post(endpoint, payload) { response }
+        def priorRequests = Seq(request)
+
+        "return status 400" in {
+          priorResponse.status mustEqual 400
+        }
+
+        "return a JSON object containing the expected message" in {
+          priorResponse.body must /("message" -> "Invalid user request.")
+          priorResponse.body must /("data") / "Password shorter than 6 characters."
         }
       }
     }
 
-    "when the password does not contain uppercase" should {
-      "return status 400 and the correct message" in {
+    "when the password does not contain any uppercase characters should" >> inline {
+
+      new Context.PriorRequestsClean {
         val payload = toByteArray(UserRequest("yeah", "mail@mail.com", "mypass123", "mypass123"))
-        post(baseEndpoint, payload) {
-          status mustEqual 400
-          body must /("message" -> "Invalid user request.")
-          body must /("data") / "Password does not contain a mixture of lower case(s), upper case(s), and number(s)."
+        def request = () => post(endpoint, payload) { response }
+        def priorRequests = Seq(request)
+
+        "return status 400" in {
+          priorResponse.status mustEqual 400
+        }
+
+        "return a JSON object containing the expected message" in {
+          priorResponse.body must /("message" -> "Invalid user request.")
+          priorResponse.body must /("data") /
+            "Password does not contain a mixture of lower case(s), upper case(s), and number(s)."
         }
       }
     }
 
-    "when the password does not contain lowercase" should {
-      "return status 400 and the correct message" in {
+    "when the password does not contain any lowercase characters should" >> inline {
+
+      new Context.PriorRequestsClean {
         val payload = toByteArray(UserRequest("yeah", "mail@mail.com", "MYPASS123", "MYPASS123"))
-        post(baseEndpoint, payload) {
-          status mustEqual 400
-          body must /("message" -> "Invalid user request.")
-          body must /("data") / "Password does not contain a mixture of lower case(s), upper case(s), and number(s)."
+        def request = () => post(endpoint, payload) { response }
+        def priorRequests = Seq(request)
+
+        "return status 400" in {
+          priorResponse.status mustEqual 400
+        }
+
+        "return a JSON object containing the expected message" in {
+          priorResponse.body must /("message" -> "Invalid user request.")
+          priorResponse.body must /("data") /
+            "Password does not contain a mixture of lower case(s), upper case(s), and number(s)."
         }
       }
     }
 
-    "when the password does not contain numbers" should {
-      "return status 400 and the correct message" in {
-        val payload = toByteArray(UserRequest("yeah", "mail@mail.com", "Mypass", "Mypass"))
-        post(baseEndpoint, payload) {
-          status mustEqual 400
-          body must /("message" -> "Invalid user request.")
-          body must /("data") / "Password does not contain a mixture of lower case(s), upper case(s), and number(s)."
+    "when the password does not contain any numeric characters should" >> inline {
+
+      new Context.PriorRequestsClean {
+        val payload = toByteArray(UserRequest("yeah", "mail@mail.com", "MyPass", "MyPass"))
+        def request = () => post(endpoint, payload) { response }
+        def priorRequests = Seq(request)
+
+        "return status 400" in {
+          priorResponse.status mustEqual 400
+        }
+
+        "return a JSON object containing the expected message" in {
+          priorResponse.body must /("message" -> "Invalid user request.")
+          priorResponse.body must /("data") /
+            "Password does not contain a mixture of lower case(s), upper case(s), and number(s)."
         }
       }
     }
 
-    "when the requested user ID already exists in the database" should {
-      "return status 409 and the correct message" in {
-        val userRequest = UserRequest("yeah", "mail@mail.com", "Mypass123", "Mypass123")
-        val payload = toByteArray(userRequest)
-        post(baseEndpoint, payload) {
-          status mustEqual 409
-          body must /("message" -> "User ID already taken.")
-        } before {
-          servlet.users.addUser(userRequest.user)
-        } after { resetDatabase() }
+    "when the requested user ID already exists should" >> inline {
+
+      new Context.PriorRequestsClean {
+        val payload = toByteArray(UserRequest("yeah", "mail@mail.com", "MyPass123", "MyPass123"))
+        def request = () => post(endpoint, payload) { response }
+        def priorRequests = Seq(request, request)
+
+        "return status 409" in {
+          priorResponses.last.status mustEqual 409
+        }
+
+        "return a JSON object containing the expected message" in {
+          priorResponses.last.body must /("message" -> "User ID already taken.")
+        }
       }
     }
   }
