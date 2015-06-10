a55387dd0a0632fe1a249f1f1675e128192cede3
bow <bow@bow.web.id>
2015-06-10 17:55:03 +0200

    Add case class for aggregated Gentrap alignment statistics

--- src/main/scala/nl/lumc/sasc/sentinel/processors/gentrap/GentrapAlignmentStats.scala
+++ src/main/scala/nl/lumc/sasc/sentinel/processors/gentrap/GentrapAlignmentStats.scala
@@ -1,22 +1,43 @@
 package nl.lumc.sasc.sentinel.processors.gentrap
 
+import nl.lumc.sasc.sentinel.models.AggrStat
+
 case class GentrapAlignmentStats(
   nReads: Long,
   nReadsAligned: Long,
   rateReadsMismatch: Double,
   rateIndel: Double,
-  pctChimeras: Option[Double] = None,
-  nSingletons: Option[Long] = None,
-  maxInsertSize: Option[Long] = None,
-  medianInsertSize: Option[Long] = None,
-  stdevInsertSize: Option[Double] = None,
   nBasesAligned: Long,
   nBasesUtr: Long,
   nBasesCoding: Long,
   nBasesIntron: Long,
   nBasesIntergenic: Long,
-  nBasesRibosomal: Option[Long],
   median5PrimeBias: Double,
   median3PrimeBias: Double,
-  normalizedTranscriptCoverage: Seq[Double])
+  normalizedTranscriptCoverage: Seq[Double],
+  nBasesRibosomal: Option[Long] = None,
+  pctChimeras: Option[Double] = None,
+  nSingletons: Option[Long] = None,
+  maxInsertSize: Option[Long] = None,
+  medianInsertSize: Option[Long] = None,
+  stdevInsertSize: Option[Double] = None)
 
+// TODO: generate the aggregate stats programmatically (using macros?)
+case class GentrapAlignmentAggregateStats(
+  nReads: AggrStat,
+  nReadsAligned: AggrStat,
+  rateReadsMismatch: AggrStat,
+  rateIndel: AggrStat,
+  nBasesAligned: AggrStat,
+  nBasesUtr: AggrStat,
+  nBasesCoding: AggrStat,
+  nBasesIntron: AggrStat,
+  nBasesIntergenic: AggrStat,
+  median5PrimeBias: AggrStat,
+  median3PrimeBias: AggrStat,
+  nBasesRibosomal: Option[AggrStat] = None,
+  pctChimeras: Option[AggrStat] = None,
+  nSingletons: Option[AggrStat] = None,
+  maxInsertSize: Option[AggrStat] = None,
+  medianInsertSize: Option[AggrStat] = None,
+  stdevInsertSize: Option[AggrStat] = None)
