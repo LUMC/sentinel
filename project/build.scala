4022c449f73d37dad4d6bd8c2f00a468d91bf5b6
bow <bow@bow.web.id>
2015-06-17 16:18:13 +0200

    Add license template for source files.

--- project/build.scala
+++ project/build.scala
@@ -3,6 +3,7 @@ import Keys._
 import org.scalatra.sbt._
 import com.earldouglas.xwp.XwpPlugin._
 import com.typesafe.sbt.SbtScalariform._
+import de.heikoseeberger.sbtheader._
 
 object SentinelBuild extends Build {
 
@@ -38,54 +39,77 @@ object SentinelBuild extends Build {
       .setPreference(SpacesWithinPatternBinders, true)
   }
 
+  lazy val headerSettings = Seq(HeaderPlugin.autoImport.headers := Map(
+    "scala" -> (
+      HeaderPattern.cStyleBlockComment,
+      """|/**
+        | * Copyright (c) 2015 Leiden University Medical Center and contributors
+        | *                    (see AUTHORS.md file for details).
+        | *
+        | * Licensed under the Apache License, Version 2.0 (the "License");
+        | * you may not use this file except in compliance with the License.
+        | * You may obtain a copy of the License at
+        | *
+        | * http://www.apache.org/licenses/LICENSE-2.0
+        | *
+        | * Unless required by applicable law or agreed to in writing, software
+        | * distributed under the License is distributed on an "AS IS" BASIS,
+        | * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        | * See the License for the specific language governing permissions and
+        | * limitations under the License.
+        | */
+        |""".stripMargin
+      )))
+
   lazy val jettyRunnerSettings = jetty(Seq(JettyRunnerModule))
 
-  lazy val projectSettings = ScalatraPlugin.scalatraWithJRebel ++ scalariformSettings ++ jettyRunnerSettings ++ Seq(
-    organization := Organization,
-    name := Name,
-    version := Version,
-    initialize := {
-      val _ = initialize.value
-      if (sys.props("java.specification.version") != JavaVersion)
-        sys.error("Sentinel requires Java 8.")
-    },
-    scalaVersion := ScalaVersion,
-    scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature"),
-    testOptions in Test += Tests.Argument("console", "junitxml"),
-    testOptions in IntegrationTest += Tests.Argument("console", "junitxml"),
-    ivyScala := ivyScala.value map { _.copy(overrideScalaVersion = true) },
-    resolvers += Classpaths.typesafeReleases,
-    resolvers += "Local Maven Repository" at "file://"+Path.userHome.absolutePath+"/.m2/repository",
-    resolvers += "Sonatype OSS Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/",
-    resolvers += "Sonatype OSS Releases" at "http://oss.sonatype.org/content/repositories/releases/",
-    ScalariformKeys.preferences := formattingPreferences,
-    dependencyOverrides ++= Set(JettyRunnerModule),
-    libraryDependencies ++= Seq(
-      "ch.qos.logback"          %  "logback-classic"          % "1.1.2"               % "runtime",
-      "com.github.fge"          %  "json-schema-validator"    % "2.2.6",
-      "com.novus"               %% "salat"                    % "1.9.9",
-      "com.typesafe.akka"       %% "akka-actor"               % "2.3.6",
-      "commons-codec"           %  "commons-codec"            % "1.7",
-      "commons-io"              %  "commons-io"               % "2.4",
-      "de.flapdoodle.embed"     %  "de.flapdoodle.embed.mongo"% "1.47.3"              % "test;it",
-      "javax.servlet"           %  "javax.servlet-api"        % "3.1.0"               % "container;provided;test;it",
-      "net.databinder.dispatch" %% "dispatch-core"            % "0.11.2",
-      "net.databinder.dispatch" %% "dispatch-json4s-jackson"  % "0.11.2",
-      "org.eclipse.jetty"       %  "jetty-plus"               % JettyVersion          % "container",
-      "org.eclipse.jetty"       %  "jetty-webapp"             % JettyVersion          % "container",
-      "org.json4s"              %% "json4s-jackson"           % Json4sVersion,
-      "org.json4s"              %% "json4s-mongo"             % Json4sVersion,
-      "org.json4s"              %% "json4s-ext"               % Json4sVersion,
-      "org.mongodb"             %% "casbah"                   % "2.8.0",
-      "org.mindrot"             %  "jbcrypt"                  % "0.3m",
-      "org.scalatra"            %% "scalatra"                 % ScalatraVersion,
-      "org.scalatra"            %% "scalatra-specs2"          % ScalatraVersion       % "test;it",
-      "org.scalatra"            %% "scalatra-json"            % ScalatraVersion,
-      "org.scalatra"            %% "scalatra-swagger"         % ScalatraVersion,
-      "org.scalatra"            %% "scalatra-swagger-ext"     % ScalatraVersion,
-      "org.scalatra"            %% "scalatra-slf4j"           % ScalatraVersion
+  lazy val projectSettings = ScalatraPlugin.scalatraWithJRebel ++ scalariformSettings ++ jettyRunnerSettings ++
+    headerSettings ++ Seq(
+      organization := Organization,
+      name := Name,
+      version := Version,
+      initialize := {
+        val _ = initialize.value
+        if (sys.props("java.specification.version") != JavaVersion)
+          sys.error("Sentinel requires Java 8.")
+      },
+      scalaVersion := ScalaVersion,
+      scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature"),
+      testOptions in Test += Tests.Argument("console", "junitxml"),
+      testOptions in IntegrationTest += Tests.Argument("console", "junitxml"),
+      ivyScala := ivyScala.value map { _.copy(overrideScalaVersion = true) },
+      resolvers += Classpaths.typesafeReleases,
+      resolvers += "Local Maven Repository" at "file://"+Path.userHome.absolutePath+"/.m2/repository",
+      resolvers += "Sonatype OSS Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/",
+      resolvers += "Sonatype OSS Releases" at "http://oss.sonatype.org/content/repositories/releases/",
+      ScalariformKeys.preferences := formattingPreferences,
+      dependencyOverrides ++= Set(JettyRunnerModule),
+      libraryDependencies ++= Seq(
+        "ch.qos.logback"          %  "logback-classic"          % "1.1.2"               % "runtime",
+        "com.github.fge"          %  "json-schema-validator"    % "2.2.6",
+        "com.novus"               %% "salat"                    % "1.9.9",
+        "com.typesafe.akka"       %% "akka-actor"               % "2.3.6",
+        "commons-codec"           %  "commons-codec"            % "1.7",
+        "commons-io"              %  "commons-io"               % "2.4",
+        "de.flapdoodle.embed"     %  "de.flapdoodle.embed.mongo"% "1.47.3"              % "test;it",
+        "javax.servlet"           %  "javax.servlet-api"        % "3.1.0"               % "container;provided;test;it",
+        "net.databinder.dispatch" %% "dispatch-core"            % "0.11.2",
+        "net.databinder.dispatch" %% "dispatch-json4s-jackson"  % "0.11.2",
+        "org.eclipse.jetty"       %  "jetty-plus"               % JettyVersion          % "container",
+        "org.eclipse.jetty"       %  "jetty-webapp"             % JettyVersion          % "container",
+        "org.json4s"              %% "json4s-jackson"           % Json4sVersion,
+        "org.json4s"              %% "json4s-mongo"             % Json4sVersion,
+        "org.json4s"              %% "json4s-ext"               % Json4sVersion,
+        "org.mongodb"             %% "casbah"                   % "2.8.0",
+        "org.mindrot"             %  "jbcrypt"                  % "0.3m",
+        "org.scalatra"            %% "scalatra"                 % ScalatraVersion,
+        "org.scalatra"            %% "scalatra-specs2"          % ScalatraVersion       % "test;it",
+        "org.scalatra"            %% "scalatra-json"            % ScalatraVersion,
+        "org.scalatra"            %% "scalatra-swagger"         % ScalatraVersion,
+        "org.scalatra"            %% "scalatra-swagger-ext"     % ScalatraVersion,
+        "org.scalatra"            %% "scalatra-slf4j"           % ScalatraVersion
+      )
     )
-  )
 
   lazy val project = Project("sentinel",  file("."), settings = projectSettings)
     .configs(IntegrationTest)
