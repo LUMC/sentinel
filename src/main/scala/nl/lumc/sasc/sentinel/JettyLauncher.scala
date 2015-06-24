02105486efbdf3b48ae2f704bd4ee403fb450665
bow <bow@bow.web.id>
2015-06-24 16:35:23 +0200

    Add initial embedded Jetty launcher

---
+++ src/main/scala/nl/lumc/sasc/sentinel/JettyLauncher.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2015 Leiden University Medical Center and contributors
+ *                    (see AUTHORS.md file for details).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.lumc.sasc.sentinel
+
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.servlet.DefaultServlet
+import org.eclipse.jetty.webapp.WebAppContext
+import org.scalatra.servlet.ScalatraListener
+
+object JettyLauncher {
+
+  def main(args: Array[String]) {
+
+    val port = if (System.getenv("PORT") != null) System.getenv("PORT").toInt else 8080
+    val server = new Server(port)
+    val context = new WebAppContext()
+
+    context setContextPath "/"
+    context.setResourceBase("src/main/webapp")
+    context.addEventListener(new ScalatraListener)
+    context.addServlet(classOf[DefaultServlet], "/")
+    server.setHandler(context)
+    server.start()
+    server.join()
+  }
+}
