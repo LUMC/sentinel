483cd8e980cebf54234ac980c42781151652bf56
bow <bow@bow.web.id>
2015-10-07 10:25:50 +0200

    Add object for stacking \/[_, _] and Future[_] in FutureAdapter
    This will be used for the for-comprehensions involving both Monads.

--- sentinel/src/main/scala/nl/lumc/sasc/sentinel/adapters/FutureAdapter.scala
+++ sentinel/src/main/scala/nl/lumc/sasc/sentinel/adapters/FutureAdapter.scala
@@ -19,7 +19,7 @@ package nl.lumc.sasc.sentinel.adapters
 import scala.concurrent.duration._
 import scala.concurrent.{ ExecutionContext, Future }
 
-import scalaz._
+import scalaz._, Scalaz._
 
 /**
  * Base adapter that uses Scala's Futures.
@@ -33,5 +33,57 @@ trait FutureAdapter {
   implicit def futureMonad(implicit context: ExecutionContext) = new Monad[Future] {
     override def point[T](t: => T): Future[T] = Future { t }
     override def bind[T, U](ft: Future[T])(f: T => Future[U]): Future[U] = ft.flatMap(f)
+  }
+
+  /**
+   * For easier access to Scalaz's `Monoid[_]` conversion for subclasses.
+   * See: https://groups.google.com/forum/#!topic/scalaz/9SJbGlpS7Kw
+   */
+  protected final implicit def lMonoid[A] = listMonoid[A]
+
+  /**
+   * Helper object for stacking the disjunction (`\/`) and `Future` monads.
+   *
+   * Without this object, we have to wrap methods returning Future[A], List[String] \/ A, or A manually in EitherT:
+   *
+   * {{{
+   *   def f1(): Future[T] = ...
+   *   def f2(): List[String] \/ T = ...
+   *   def f3(): T = ...
+   *
+   *   val result = for {
+   *       a <- EitherT(Future { f1() })
+   *       b <- EitherT(Future.successful(f2()))
+   *       c <- EitherT(Future { f3().right[List[String]] })
+   *   } yield c
+   * }}}
+   *
+   * With this object in scope, we can do the following:
+   *
+   * {{{
+   *   val result = for {
+   *       a <- ? <~ f1()
+   *       b <- ? <~ f2()
+   *       c <- ? <~ f3()
+   *   } yield c
+   * }}}
+   *
+   * In both cases, the type of `result` is EitherT[Future, List[String], T], so its `run` method must still be invoked
+   * to obtain the final value.
+   *
+   * Adapted from: http://www.47deg.com/blog/fp-for-the-average-joe-part-2-scalaz-monad-transformers
+   */
+  object ? {
+
+    /** Type alias for our stacked monads. */
+    type Result[T] = EitherT[Future, List[String], T]
+
+    def <~[T](v: Future[List[String] \/ T]): Result[T] = EitherT(v)
+
+    def <~[T](v: List[String] \/ T): Result[T] = EitherT(Future.successful(v))
+
+    def <~[T](v: Future[T])(implicit context: ExecutionContext): Result[T] = EitherT(v.map(_.right[List[String]]))
+
+    def <~[T](v: T)(implicit context: ExecutionContext): Result[T] = v.point[Result]
   }
 }
