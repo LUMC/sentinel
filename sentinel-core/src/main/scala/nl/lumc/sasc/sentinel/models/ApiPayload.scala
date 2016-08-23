370c40213d87a776f016d34314ad93122b6d3a48
bow <bow@bow.web.id>
2016-08-23 11:08:57 +0200

    Add documentation for predefined ApiPayload objects

--- sentinel-core/src/main/scala/nl/lumc/sasc/sentinel/models/ApiPayload.scala
+++ sentinel-core/src/main/scala/nl/lumc/sasc/sentinel/models/ApiPayload.scala
@@ -59,18 +59,21 @@ object ApiPayload {
 /** Common API messages. */
 object Payloads {
 
+  /** Returned when a run summary is already uploaded, HTTP 409. */
   object DuplicateSummaryError {
     def message = "Run summary already uploaded."
     def apply(existingId: String) =
       ApiPayload(message, List(s"Existing ID: $existingId."), (ap: ApiPayload) => Conflict(ap))
   }
 
+  /** Returned when a user ID is already taken, HTTP 409. */
   object DuplicateUserIdError {
     def message = "User ID already taken."
     def apply(existingId: String) =
       ApiPayload(message, List(s"Existing ID: $existingId."), (ap: ApiPayload) => Conflict(ap))
   }
 
+  /** Returned for any generic database error, HTTP 500. */
   object UnexpectedDatabaseError {
     def message = "Unexpected database error."
     def apply(hint: String) = ApiPayload(message, hints = List(hint))
@@ -78,8 +81,10 @@ object Payloads {
     def apply() = ApiPayload(message)
   }
 
+  /** Returned when a run summary deletion is incomplete, HTTP 500. */
   val IncompleteDeletionError = UnexpectedDatabaseError("Deletion incomplete.")
 
+  /** Trait for validation errors from payloads sent by the user. */
   trait ValidationErrorLike {
     def message: String
     protected val func = (ap: ApiPayload) => BadRequest(ap)
@@ -88,10 +93,12 @@ object Payloads {
     final def apply() = ApiPayload(message, httpFunc = func)
   }
 
+  /** Returned when a JSON validation error occurs. */
   object JsonValidationError extends ValidationErrorLike {
     def message = "JSON is invalid."
   }
 
+  /** Returned when a JSON patch validation error occurs. */
   object PatchValidationError extends ValidationErrorLike {
     def message = "Invalid patch operation(s)."
     def apply(patch: JsonPatch.PatchOp) = patch match {
@@ -114,11 +121,13 @@ object Payloads {
 
   }
 
+  /** Returned when an invalid database ID is specified. HTTP 400. */
   object InvalidDbError {
     def message = "Invalid ID(s) provided."
     def apply(invalidIds: List[String]) = ApiPayload(message, invalidIds, (ap: ApiPayload) => BadRequest(ap))
   }
 
+  /** Returned when an invalid pipeline name is specified. HTTP 400. */
   object InvalidPipelineError {
     def message = "Pipeline parameter is invalid."
     def apply(validList: Seq[String]) = ApiPayload(
@@ -128,32 +137,40 @@ object Payloads {
     )
   }
 
+  /** Returned when an invalid library parameter is specified. HTTP 400. */
   val InvalidLibError = ApiPayload(
     "Library type parameter is invalid.",
     List("Valid values are '" + LibType.values.toList.map(_.toString).sorted.mkString("', '") + "'."),
     (ap: ApiPayload) => BadRequest(ap)
   )
 
+  /** Returned when an invalid accumulation level parameter is specified. HTTP 400. */
   val InvalidAccLevelError = ApiPayload(
     "Accumulation level parameter is invalid.",
     List("Valid values are '" + AccLevel.values.toList.map(_.toString).sorted.mkString("', '") + "'."),
     (ap: ApiPayload) => BadRequest(ap)
   )
 
+  /** Returned when an invalid sequencing QC phase parameter is specified. HTTP 400. */
   val InvalidSeqQcPhaseError = ApiPayload(
     "Sequencing QC phase parameter is invalid.",
     List("Valid values are '" + SeqQcPhase.values.toList.map(_.toString).sorted.mkString("', '") + "'."),
     (ap: ApiPayload) => BadRequest(ap)
   )
 
+  /** Returned when a resource is already deleted. HTTP 410. */
   val ResourceGoneError = ApiPayload("Resource already deleted.", httpFunc = (ap: ApiPayload) => Gone(ap))
 
+  /** Returned when a required user ID URL parameter is not specified. HTTP 400. */
   val UnspecifiedUserIdError = ApiPayload("User ID not specified.", httpFunc = (ap: ApiPayload) => BadRequest(ap))
 
+  /** Returned when a required run ID URL parameter is not specified. HTTP 400. */
   val UnspecifiedRunIdError = ApiPayload("Run summary ID not specified.", httpFunc = (ap: ApiPayload) => BadRequest(ap))
 
+  /** Returned when a required pipeline name URL parameter is not specified. HTTP 400. */
   val UnspecifiedPipelineError = ApiPayload("Pipeline not specified.", httpFunc = (ap: ApiPayload) => BadRequest(ap))
 
+  /** Returned when a specified user ID URL parameter points to a nonexistent user ID. HTTP 404. */
   object UserIdNotFoundError {
     protected def func = (ap: ApiPayload) => NotFound(ap)
     def message = "User ID can not be found."
@@ -161,22 +178,28 @@ object Payloads {
     def apply() = ApiPayload(message, httpFunc = func)
   }
 
+  /** Returned when a specified run ID URL parameter points to a nonexistent run ID. HTTP 404. */
   val RunIdNotFoundError = ApiPayload("Run summary ID can not be found.", httpFunc = (ap: ApiPayload) => NotFound(ap))
 
+  /** Returned when no data points for aggregation exist. HTTP 404. */
   val DataPointsNotFoundError = ApiPayload("No data points for aggregation found.", httpFunc = (ap: ApiPayload) => NotFound(ap))
 
+  /** Returned when authentication is not provided or is provided but does not match. HTTP 401. */
   val AuthenticationError = ApiPayload(
     "Authentication required to access resource.",
     httpFunc = (ap: ApiPayload) => Unauthorized(ap)
   )
 
+  /** Returned when an authentication is provided but does not match. HTTP 401. */
   val OptionalAuthenticationError = ApiPayload(
     "User ID and/or API key is provided but authentication failed.",
     httpFunc = (ap: ApiPayload) => Unauthorized(ap)
   )
 
+  /** Returned when an authenticated user is known not to have access to the resource. HTTP 403. */
   val AuthorizationError = ApiPayload("Unauthorized to access resource.", httpFunc = (ap: ApiPayload) => Forbidden(ap))
 
+  /** Returned when a run summary object is too large for upload. HTTP 413. */
   object RunSummaryTooLargeError {
     def message = "Run summary exceeded maximum allowed size."
     def apply(maxRunSummarySize: Long) =
@@ -187,5 +210,6 @@ object Payloads {
       )
   }
 
+  /** Returned when any unexpected errors occur. HTTP 500. */
   val UnexpectedError = ApiPayload("Unexpected error.", hints = List("Please contact the site administrators."))
 }
